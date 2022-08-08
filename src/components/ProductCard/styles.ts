d616f2b4319dfc9adf77ba907d5a4d13943c9448
Omar <omarucolombari@gmail.com>
2022-08-08 17:51:15 -0300

    Feat: criado e estilizado component ProductCard

---
+++ src/components/ProductCard/styles.ts
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 312px;
+  height: 350px;
+  :hover {
+    figure {
+      outline: ${(props) => props.theme.colors.brand1} 2px solid;
+    }
+  }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const Title = styled.h3`
+  color: ${(props) => props.theme.colors.grey1};
+  font: ${(props) => props.theme.fonts["heading-7-600"]};
+  max-height: 22px;
+  width: 98%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const Description = styled.p`
+  font: ${(props) => props.theme.fonts["body-2-400"]};
+  color: ${(props) => props.theme.colors.grey2};
+`;
+
+export const Figure = styled.figure`
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.grey7};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Avatar = styled.div`
+  color: ${(props) => props.theme.colors.whiteFixed};
+  font: ${(props) => props.theme.fonts["body-2-500"]};
+  border-radius: 100%;
+  background-color: ${(props) => props.theme.colors.brand1};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 32px;
+`;
+
+export const Advertiser = styled.div`
+  span {
+    font: ${(props) => props.theme.fonts["body-2-500"]};
+    color: ${(props) => props.theme.colors.grey2};
+    margin-left: 8px;
+  }
+  display: flex;
+  align-items: center;
+`;
+
+export const ContainerTags = styled.div`
+  button {
+    padding: 5px;
+  }
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+`;
+
+export const CardFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 90%;
+`;
+
+export const Price = styled.span`
+  font: ${(props) => props.theme.fonts["heading-7-500"]};
+  color: ${(props) => props.theme.colors.grey1};
+`;
