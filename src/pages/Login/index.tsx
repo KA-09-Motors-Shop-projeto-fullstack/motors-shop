bb5f01d6a7b5b0e2a9dfaa7bde8f5c5a3881742c
Omar <omarucolombari@gmail.com>
2022-08-19 15:32:43 -0300

    Refactor: excluido component Login e adicionado seu codigo na page Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,20 +1,75 @@
-import React from "react";
 import { Redirect } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import Login from "../../components/Login";
 import { isAuthenticated } from "../../services/auth";
 import { Main } from "./styles";
+import React, { useContext } from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Container, Form } from "./styles";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../../providers/Users";
+import { UserContextType } from "../../@types/users";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 const LoginPage: React.FC = () => {
-  if (isAuthenticated()) {
-    return <Redirect to="profile" />;
-  }
+  const history = useHistory();
+  const { loginUser } = useContext(UserContext) as UserContextType;
+  const goSignup = () => history.push("/signup");
+
+  const schema = yup.object().shape({
+    email: yup.string().email("Email inválido").required("Email obrigatório"),
+    password: yup.string().required("Password obrigatório"),
+  });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data: any) => {
+    await loginUser(data);
+  };
+
+  if (isAuthenticated()) return <Redirect to="profile" />;
+
   return (
     <>
       <Header />
       <Main>
-        <Login />
+        <Container>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <h2>Login</h2>
+            <Input
+              register={register}
+              label="Email"
+              name="email"
+              placeholder="Digitar usuário"
+              error={!!errors.email?.message}
+            />
+            <Input
+              register={register}
+              label="Senha"
+              name="password"
+              type="password"
+              placeholder="Digitar senha"
+              error={!!errors.password?.message}
+            />
+            <span>Esqueci minha senha</span>
+            <Button type="submit" typeButton="brand1" typeFont="big">
+              Entrar
+            </Button>
+            <span>Ainda não possui conta?</span>
+            <Button onClick={goSignup} typeButton="outline2" typeFont="big">
+              Cadastrar
+            </Button>
+          </Form>
+        </Container>
       </Main>
       <Footer />
     </>
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -5,3 +5,46 @@ export const Main = styled.main`
   padding-top: 50px;
   padding-bottom: 190px;
 `;
+
+export const Container = styled.div`
+  min-width: 343px;
+  width: 30%;
+  height: 594px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  background-color: ${(props) => props.theme.colors.whiteFixed};
+  h2 {
+    font: ${(props) => props.theme.fonts["heading-5-500"]};
+    align-self: flex-start;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  height: 90%;
+  justify-content: space-around;
+  span {
+    font: ${(props) => props.theme.fonts["body-2-500"]};
+    color: ${(props) => props.theme.colors.grey2};
+    align-self: flex-end;
+    :last-of-type {
+      font: ${(props) => props.theme.fonts["body-2-400"]};
+      align-self: center;
+    }
+  }
+  button,
+  input {
+    width: 100%;
+  }
+  div {
+    &:nth-child(2),
+    &:nth-child(3) {
+      width: 100%;
+    }
+  }
+`;
