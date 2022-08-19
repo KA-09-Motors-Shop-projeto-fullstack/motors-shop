a5cbd662c22994b17569293d142b3bd752a2560e
Gabriel Castedo <gabriel.castedo1@gmail.com>
2022-08-19 17:48:00 -0400

    feature: productpage css

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Switch, Route } from "react-router-dom";
 import PrivateRoute from "../components/PrivateRoute";
 import Profile from "../pages/Profile";
 import { HomePage } from "../pages/Homepage";
+import { ProductPage } from "../pages/Productpage";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      
+      <Route exact path="/product" component={ProductPage} />
+
       <PrivateRoute path="/profile">
         <Profile />
       </PrivateRoute>
