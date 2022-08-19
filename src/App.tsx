ab85341ec47a3f9add463d47bb58ccb41fe31132
Omar <omarucolombari@gmail.com>
2022-08-19 15:07:37 -0300

    Feat: adicionado ToastContainer ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Routes from "./routes";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer autoClose={2000} />
       <Routes />
     </div>
   );
